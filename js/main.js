623dafe2a85e455e175f5754a77899d83e370af4
SashaKalinin <sashakalininweb@gmail.com>
2020-05-31 14:23:11 +0300

    final

--- js/main.js
+++ js/main.js
@@ -24,27 +24,71 @@ function sendRequest(method, url) {
 }
 
 
-
-sendRequest('GET', requestURL)
-    .then(data => generateData(data.results))
-    .catch(err => console.log(err))
-
-    // let array = [];
-    // let str = 'star';
-    // let spliter = str.split('');
-    // spliter.forEach((item) => array.push(item))
-    // console.log(array)
-
+//INIT FUNCTION
 function generateData(data) {
+    arr = [];
+    localStorage.clear();
     for (let item of data) {
         arr.push(item);
+    }
+    localStorage.setItem('StarWars', JSON.stringify(arr));
+    //SORT
+    if (inputText.value) {
+        let newArr = arr.filter((elem) => {
+           return elem.name.toLowerCase().includes(inputText.value.toLowerCase());
+        })
+        heroCart.innerHTML = '';
+        newArr.forEach((item) => {
             heroCart.innerHTML +=
-                `<div class="cart" data-toggle="modal" data-target="#exampleModal">
-                 <p class="p_text" id="cardId">${item.name}</p>
-            </div>`
-        
+                `<p class="p_text" id="cardId" data-toggle="modal" data-target="#exampleModal">${item.name}</p>`
+        })
+    } 
+    else {
+        heroCart.innerHTML = '';
+        arr.forEach((item) => {
+            heroCart.innerHTML +=
+            `<p class="p_text" id="cardId" data-toggle="modal" data-target="#exampleModal">${item.name}</p>`
+        })
     }
 }
+
+//MAIN FUNCTION
+if(localStorage.key(0) === "StarWars") {
+    let localStorageData = JSON.parse(localStorage.getItem('StarWars'));
+    generateData(localStorageData);
+}else {
+    sendRequest('GET', requestURL)
+    .then(data => generateData(data.results))
+    .catch(err => console.log(err))
+}
+
+
+
+
+
+//INPUT SERCH
+inputText.addEventListener('keydown', function () {
+    if(localStorage.key(0) === "StarWars") {
+        let localStorageData = JSON.parse(localStorage.getItem('StarWars'));
+        generateData(localStorageData);
+    }else {
+        sendRequest('GET', requestURL)
+        .then(data => generateData(data.results))
+        .catch(err => console.log(err))
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -53,7 +97,7 @@ function generateData(data) {
 
 
 heroCart.addEventListener('click', function (e) {
-    
+
     for (let i = 0; i < arr.length; i++) {
         if (e.target.innerHTML == arr[i].name) {
             exampleModalLable.innerHTML = arr[i].name;
@@ -70,8 +114,26 @@ heroCart.addEventListener('click', function (e) {
 
 
 
-inputText.addEventListener('keydown', function () {
-    sendRequest('GET', requestURL)
-        .then(data => generateData(data.results))
-        .catch(err => console.log(err))
-})
+
+
+
+
+
+
+// //PARENT NODE ARR
+// let arrayData = []; 
+// function perentElem() {
+// let num = 0; 
+// $('#hero_cart').each(function(){ 
+//     let oneDiv = $(this);
+//     let countSpan = oneDiv.find('p#cardId').length - 1; 
+//     let spansData = []; 
+//     for(i = 0; i <= countSpan; i++){ 
+//         let dataSpan = oneDiv.find('p#cardId').eq(i).html(); 
+//         spansData[i] = dataSpan; 
+//     }
+//     arrayData[num] = spansData; 
+//     num ++; 
+// });
+// console.log(arrayData)
+// }
